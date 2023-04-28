94075d6f966028f23cd252abc5e90f0c1c060bab
tykman <nizom.ra@inbox.ru>
2023-04-29 01:44:37 +0500

    Домашнее задание. Skypro-arrays.

--- src/Main.java
+++ src/Main.java
@@ -4,104 +4,85 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1");
+        int[] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
 
-        int[] arr = new int[]{1, 2, 3};
+        double[] arr2 = {1.57, 7.654, 9.986};
 
-        for (int i = 1; i <= arr.length; i++) {
-            System.out.println(i);
-        }
+        double[] arr3 = {3.21, 52.25, 7.13489};
         System.out.println();
 
-        double[] oneSevenNine = {1.57, 7.654, 9.986};
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(oneSevenNine[i]);
-        }
-        System.out.println();
+       System.out.println("Задача 2");
 
-        double[] floatingPoint = new double[3];
-        floatingPoint[0] = 3.21;
-        floatingPoint[1] = 52.25;
-        floatingPoint[2] = 7.13489;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(floatingPoint[i]);
-        }
-        System.out.println();
+       int[] arr = {1,2,3};
 
-        System.out.println("Задача 2");
+       for (int i = 0; i < arr.length; i++) {
+           System.out.print(arr[i]);
+           if (i != arr.length - 1) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        int[] num = new int[]{1,2,3} ;
+       for (int i = 0; i < arr2.length; i++) {
+           System.out.print(arr2[i]);
+           if (i != arr2.length - 1) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        for (int i = 1; i <= num.length; i++) {
-            if (i == num.length - 1) {
-                System.out.println(Arrays.toString(num));
-                break;
-            }
-        }
+       for (int i = 0; i < arr3.length; i++) {
+           System.out.print(arr3[i]);
+           if (i != arr3.length - 1) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        double[] numberWithPoint = {1.57, 7.654, 9.986};
+       System.out.println("Задача 3");
 
-        for (int i = 0; i < numberWithPoint.length; i++) {
-            if (i == numberWithPoint.length - 1) {
-                System.out.println(Arrays.toString(numberWithPoint));
-                break;
-            }
-        }
+       int[] array = {1,2,3};
+       double[] array1 = {1.57, 7.654, 9.986};
+       double[] arRab = {3.21, 52.25, 7.13489};
 
-        double[] floating = new double[3];
-        floating[0] = 3.21;
-        floating[1] = 52.25;
-        floating[2] = 7.13489;
+       for (int i = array.length - 1; i >= 0; i--) {
+           System.out.print(array[i]);
+           if (i != 0) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        for (int i = 0; i < floating.length; i++) {
-             if (i == floating.length - 1) {
-                 System.out.println(Arrays.toString(floating));
-                 break;
-             }
-        }
-        System.out.println();
+       for (int i = array1.length - 1; i >= 0; i--) {
+           System.out.print(array1[i]);
+           if (i != 0) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        System.out.println("Задача 3");
+       for (int i = arRab.length - 1;i >= 0; i--) {
+           System.out.print(arRab[i]);
+           if (i != 0) {
+               System.out.print(", ");
+           }
+       }
+       System.out.println();
 
-        int[] box = new int[] {3,2,1};
+       System.out.println("Задача 4");
 
-        for (int i = 0; i < box.length; i++) {
-            if (i == box.length - 1) {
-                System.out.print(Arrays.toString(box));
-                break;
-            }
-        }
-        System.out.println();
+       int[] ar = {1, 2, 3};
 
-        double [] countDown = {9.986,7.654,1.57};
+       for (int i = 0; i < ar.length; i++) {
+           if (ar[i] % 2 != 0) {
+               ar[i] += 1;
+           }
+       }
 
-        for (int i = 0; i < countDown.length; i++) {
-            if (i == countDown.length - 1) {
-                System.out.println(Arrays.toString(countDown));
-                break;
-            }
-        }
-
-        double [] floatDouble = new double[3];
-        floatDouble [0] = 7.13489;
-        floatDouble [1] = 52.25;
-        floatDouble [2] = 3.21;
-
-        for (int i = 0; i < floatDouble.length; i++) {
-            if (i == floatDouble.length - 1) {
-                System.out.print(Arrays.toString(floatDouble));
-                break;
-            }
-        }
-        System.out.println();
-
-        System.out.println("Задача 4");
-
-        int [] countUp = {0,1,2,3,4,5,6,7,8,9,10};
-
-        for (int i = 0; i < countUp.length; i++) {
-            if (countUp[i] % 2 != 0) countUp[i] += 1;
-        }
-        System.out.println(Arrays.toString(countUp));
+       System.out.println(Arrays.toString(ar));
     }
 }
